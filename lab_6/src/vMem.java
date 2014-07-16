93e8372e215ae6aec30608bc27bb74ac952ec867
Nolan Foster <nolanfoster@gmail.com>
2014-07-16 17:36:24 -0400

    (141607: error while reading to array)

--- lab_6/src/vMem.java
+++ lab_6/src/vMem.java
@@ -1,41 +1,60 @@
-import java.io.FileNotFoundException;
+
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-
 public class vMem {
-	private int index;
-	private long value;
 	private long[] array;
-	private long[] actualArray;
 	private RandomAccessFile store;
-	private int size;
 	private int VIR;
-	public vMem(int i, int j) throws IOException{
-		array = new long[VIR];
-		store = new RandomAccessFile("files", "rw");
-		for (int k = 0; k < size; k++) {
-			store.writeLong(0);
+	private long result;
+
+	public vMem(int i, int j) throws IOException {
+		VIR = j;
+		array = new long[100];
+System.out.println("Creating Virtual Memory...");
+		store = new RandomAccessFile("files/swapfile", "rw");
+		for (int k = 0; k <= i * 8; k++) {
+			store.seek(k * 8);
+			store.writeLong(1);
 		}
+
+	}
+
+	public long vRead(int low, int i, int num) throws IOException {
+	System.out.println("Reading Virtual Memory...");
+//		for (int k = 0; k < 100; k++) {
+//			store.seek((low + k) * 8);
+//			array[k] = store.readLong();
+//		
+//		}
+        int index=num-i;
+		return array[index];
+
+	}
+
+	public void vWrite(int low, int i, long val, int num) throws IOException {
 		
+		//long res=vRead(low, i, num);
+		int index =num-i;
+		array[index] = val;
+		System.out.println("Writing to Virtual Memory...");
+		store.seek(i * 8);
+		store.writeLong(array[num-i]);
+
 	}
 
 	public void put(int i, long v) throws IOException {
-		//pull 100 values from file starting from index store values in array add in the index
-		store.seek(VIR*i);
-		if(i>=0 && i<=99){
-			store.readLong();
-		}
-		index=i;
-		value=v;
-		array[i]=v;
-		
+			int low = (i / 100) * 100;
+			int high = low + 100;
+			vWrite(low, i, v, high);
 	}
 
 	public long get(int i) throws IOException {
-		store.seek(VIR*i);
+			int low = (i / 100) * 100;
+			int high = low + 100;
+			result= vRead(low, i, high);
 		
-		return array[i];
+		return result;
 	}
-	
+
 }
