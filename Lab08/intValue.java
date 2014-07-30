370c949b3e5c86f47d01022a989a3a79e90ae4cc
Nolan Foster <nolanfoster@gmail.com>
2014-07-30 10:04:07 -0400

    Finished Lab 8

---
+++ Lab08/intValue.java
@@ -0,0 +1,22 @@
+
+public class intValue {
+	
+	int number1 = 0;
+	int number2 = 0;
+	
+
+	public int multiply(int n){
+		n = number1 * number2;
+		return n;
+	}
+	
+	public int add(int n){
+		n = number1 + number2;
+		return n;
+	}
+	
+	public double pow(double n){
+		n = Math.pow(number1, number2);
+		return n;
+	}
+}
